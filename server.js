df3de9a9b25d3cfea8104c815474b15de442087a
Nicholas Peth <nbpeth@Nicholass-iMac.local>
2019-09-22 10:33:31 -0600

    app state and multisesh

--- server.js
+++ server.js
@@ -12,20 +12,63 @@ app.use(cors);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    
-    console.log('received: %s', message);
+const state = {
+  sessions: {},
+}
 
-    wss.clients
-      .forEach(client => {
-        const clientIsNotSenderOfMessage = client !== ws;
-        if (clientIsNotSenderOfMessage) {
-          client.send(message);
-        }
-      });
-  });
-});
+handleNewClients = (ws) => {
+  // new connection? ship the current app state
+  const message = formatMessage('state-of-the-state', {state: state})
+  sendCurrentState(ws, message);
+
+  ws.on('message', handleIncomingMessages);
+}
+
+handleIncomingMessages = (message) => {
+  const messageObj = JSON.parse(message);
+  const messageKey = Object.keys(messageObj)[0];
+
+  console.log('received:', messageObj);
+
+  switch (messageKey) {
+    case 'session-created':
+      createNewSession(messageObj['session-created']);
+      break;
+    default:
+      console.log('message!?', messageObj)
+  }
+
+  notifyClients(wss, messageObj);
+
+}
+
+sendCurrentState = (ws, state) => ws.send(JSON.stringify(state))
+
+notifyClients = (message) => {
+  wss.clients
+    .forEach(client => {
+      // const clientIsNotSenderOfMessage = client !== ws;
+      // if (clientIsNotSenderOfMessage) {
+        console.log('sending', JSON.stringify(message));
+      client.send(JSON.stringify(message));
+      // }
+    });
+}
+
+createNewSession = (message) => {
+  const sessionName = message && message.sessionName ? message.sessionName : undefined;
+  if(!sessionName) {
+    return;
+  }
+  state.sessions[sessionName] = {}
+  const stateMessage = formatMessage('session-created', {state: state})
+
+  notifyClients(stateMessage)
+}
+
+formatMessage = (key, messageBody) => ({[key]: messageBody});
+
+wss.on('connection', handleNewClients);
 
 server.listen(process.env.PORT || 8999, () => {
   console.log(`Server running on port ${server.address().port}`);
