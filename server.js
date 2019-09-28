013c69c28eeb1da974d41c618574183e3dcb1089
Nicholas Peth <nbpeth@Nicholass-iMac.local>
2019-09-28 12:13:06 -0600

    add sessions with their own users

--- server.js
+++ server.js
@@ -5,80 +5,118 @@ const app = express();
 const http = require('http');
 const WebSocket = require('ws');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors);
 
 const server = http.createServer(app);
-const wss = new WebSocket.Server({server});
+const wss = new WebSocket.Server({ server });
 let _ws;
 const state = {
-    sessions: {},
+  sessions: {},
 };
 
 handleNewClients = (ws) => {
-    _ws = ws;
-    // new connection? ship the current app state
-    const message = formatMessage('state-of-the-state', state);
+  _ws = ws;
+  // new connection? ship the current app state
+  const message = formatMessage('state-of-the-state', state);
 
-    notifyCaller(message);
+  notifyCaller(message);
 
-    ws.on('message', handleIncomingMessages);
+  ws.on('message', handleIncomingMessages);
 };
 
 handleIncomingMessages = (message) => {
-    const messageData = JSON.parse(message);
-    const eventType = messageData.eventType;
-    const payload = messageData.payload;
+  const messageData = JSON.parse(message);
+  const eventType = messageData.eventType;
 
-    console.log('received!!', message);
-    console.log('type:::', eventType)
+  // console.log('type:::', eventType, 'load...', payload);
 
-    switch (eventType) {
-        case 'state-of-the-state':
-            notifyCaller(formatMessage(eventType, state));
-            break;
-        case 'session-created':
-            createNewSession(payload);
-            notifyClients(formatMessage(eventType, state))
-    }
+  switch (eventType) {
+    case 'state-of-the-state':
+      notifyCaller(formatMessage(eventType, state));
+      break;
+    case 'session-created':
+      createNewSession(messageData);
+      break;
+    case 'session-state':
+      getSessionState(messageData);
+      break;
+    case 'participant-joined':
+      addParticipantToSession(messageData)
+      break;
+    case 'participant-removed':
+      removeParticipantFromSession(messageData)
+      break;
+  }
 };
 
+getSessionState = (messageData) => {
+  const eventType = messageData.eventType;
+  const payload = messageData.payload;
+  const requestedSession = payload.sessionName;
+  const sessionData = state.sessions[requestedSession];
+  notifyCaller(formatMessage(eventType, sessionData, requestedSession));
+}
+
+addParticipantToSession = (messageData) => {
+  const payload = messageData.payload;
+  const requestedSession = payload.sessionName;
+  const sessionData = state.sessions[requestedSession];
+  const participantToAdd = payload.userName;
+
+  sessionData.participants[participantToAdd] = { vote: 0 };
+
+  notifyClients(formatMessage(messageData.eventType, sessionData, requestedSession))
+}
+
+removeParticipantFromSession = (messageData) => {
+  const payload = messageData.payload;
+  const requestedSession = payload.sessionName;
+  const sessionData = state.sessions[requestedSession];
+  const participantToRemove = payload.userName;
+
+  sessionData.participants[participantToRemove] = undefined;
+
+  notifyClients(formatMessage(messageData.eventType, sessionData))
+}
+
+createNewSession = (messageData) => {
+  const payload = messageData.payload;
+  const sessionName = payload && payload.sessionName ? payload.sessionName : undefined;
+  if (!sessionName) {
+    return;
+  }
+  state.sessions[sessionName] = { participants: {} };
+  const stateMessage = formatMessage('session-created', state);
+
+  notifyClients(stateMessage)
+};
+
+formatMessage = (eventType, payload, targetSession) => ({
+  eventType: eventType,
+  payload: payload,
+  targetSession: targetSession,
+});
 
 notifyClients = (message) => {
-    wss.clients
-        .forEach(client => {
-            client.send(JSON.stringify(message));
-        });
+  wss.clients
+    .forEach(client => {
+      client.send(JSON.stringify(message));
+    });
 };
 
 notifyCaller = (message) => {
-    wss.clients
-        .forEach(client => {
-            if (client === _ws) {
-                client.send(JSON.stringify(message));
-            }
-        });
+  wss.clients
+    .forEach(client => {
+      if (client === _ws) {
+        client.send(JSON.stringify(message));
+      }
+    });
 };
-
-createNewSession = (message) => {
-    const sessionName = message && message.sessionName ? message.sessionName : undefined;
-    if (!sessionName) {
-        return;
-    }
-    state.sessions[sessionName] = {participants: {}};
-    const stateMessage = formatMessage('session-created', state);
-
-    notifyClients(stateMessage)
-};
-
-formatMessage = (eventType, payload) => ({
-    eventType: eventType,
-    payload: payload
-});
 
 wss.on('connection', handleNewClients);
 
 server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server running on port ${server.address().port}`);
+  console.log(`Server running on port ${server.address().port}`);
 });
